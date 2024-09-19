149cb12270d24710aae438ad4c59a04468aaa754
Kouyang07 <kaicheng@kouyang.dev>
2024-09-19 14:16:45 -0400

    Added limit switch

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/Intake.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/Intake.java
@@ -2,6 +2,7 @@ package org.firstinspires.ftc.teamcode.subsystems;
 
 import com.qualcomm.robotcore.hardware.HardwareMap;
 import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.hardware.DigitalChannel;
 
 public class Intake {
     private Servo armServo;
@@ -9,9 +10,15 @@ public class Intake {
     private double armServoPosition = 1; // Start at middle position
     private double intakeServoPosition = 0;
 
+    private DigitalChannel armLimitSwitch;
+
     public Intake(HardwareMap hardwareMap) {
         armServo = hardwareMap.get(Servo.class, "ArmServo");
         intakeServo = hardwareMap.get(Servo.class, "IntakeServo");
+
+        // Initialize the limit switch
+        armLimitSwitch = hardwareMap.get(DigitalChannel.class, "ArmLimitSwitch");
+        armLimitSwitch.setMode(DigitalChannel.Mode.INPUT);
 
         armServo.setPosition(armServoPosition);
         intakeServo.setPosition(intakeServoPosition);
@@ -23,6 +30,10 @@ public class Intake {
     }
 
     public void tiltArmDown() {
+        // Check if the limit switch is pressed
+        if (!armLimitSwitch.getState()) {
+            return;
+        }
         armServoPosition = Math.max(armServoPosition - 0.001, 0);
         armServo.setPosition(armServoPosition);
     }
