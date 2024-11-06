d8fb57d7312ccb40b03c20f6269f40200dd2604c
Neil Singh <singhn1@ewsdmail.org>
2024-11-06 11:56:11 -0500

    The working code for the FTC Mecanum robot. No other code only drive.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/WROP.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/WROP.java
@@ -1,25 +1,25 @@
 package org.firstinspires.ftc.teamcode;
 
+import static org.firstinspires.ftc.robotcore.external.BlocksOpModeCompanion.gamepad1;
+
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
-import org.firstinspires.ftc.teamcode.subsystems.Arm;
 import org.firstinspires.ftc.teamcode.subsystems.Drivetrain;
-import org.firstinspires.ftc.teamcode.subsystems.Intake;
 
 @TeleOp(name = "WRCode")
 public class WROP extends LinearOpMode {
 
     private Drivetrain drivetrain;
-    private Arm arm;
-    private Intake intake;
+    //private Arm arm;
+     //private Intake intake;
 
     @Override
     public void runOpMode() {
         // Initialize subsystems
         drivetrain = new Drivetrain(hardwareMap);
-        arm = new Arm(hardwareMap);
-        intake = new Intake(hardwareMap);
+        //arm = new Arm(hardwareMap);
+        //intake = new Intake(hardwareMap);
 
         // Wait for the game to start
         waitForStart();
@@ -27,10 +27,12 @@ public class WROP extends LinearOpMode {
         // Main loop
         while (opModeIsActive()) {
             // Drivetrain control
-            double speed = -gamepad1.left_stick_y;
-            double turn = -gamepad1.right_stick_x * 0.8;
-            drivetrain.drive(speed, turn);
+            double Vertical = gamepad1.left_stick_y;
+            double Horizontal = gamepad1.left_stick_x;
+            double Pivot = gamepad1.right_stick_x;
+            drivetrain.drive(Vertical, Horizontal, Pivot);
 
+            /*
             // Arm control
             if (gamepad1.y) {
                 arm.moveUp();
@@ -58,6 +60,7 @@ public class WROP extends LinearOpMode {
                     telemetry.addData("Limit Switch Status: ", "Not Pressed");
                 }
             }
+             */
             telemetry.update();
         }
     }
