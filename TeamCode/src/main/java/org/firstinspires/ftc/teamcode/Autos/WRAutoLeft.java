8c8c3fd5d6ce597d7f80860e88403e6bac4db824
Neil Singh <singhn1@ewsdmail.org>
2025-02-01 17:39:48 -0500

    Working Auto

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autos/WRAutoLeft.java
@@ -0,0 +1,2 @@
+package org.firstinspires.ftc.teamcode.Autos;public class WRAutoLeft {
+}
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autos/WRAutoRight.java
@@ -0,0 +1,74 @@
+package org.firstinspires.ftc.teamcode.Autos;
+
+import android.content.res.Resources;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+import org.firstinspires.ftc.teamcode.subsystems.ArmNormal;
+import org.firstinspires.ftc.teamcode.subsystems.Drivetrain;
+import org.firstinspires.ftc.teamcode.subsystems.Intake;
+import org.firstinspires.ftc.teamcode.subsystems.ArmTape;
+
+@Autonomous(name = "Liam Blackmail Auto")
+public class WRAuto extends LinearOpMode {
+    private Drivetrain drivetrain;
+    private Intake intake;
+    private ArmNormal arm;
+    private ArmTape armTape;
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+        // Initialize subsystems
+        drivetrain = new Drivetrain(hardwareMap);
+        intake = new Intake(hardwareMap);
+        arm = new ArmNormal(hardwareMap);
+        armTape = new ArmTape(hardwareMap);
+
+
+        // Wait for the game to start
+        waitForStart();
+
+//        drivetrain.drive(1,0,0);
+//        Thread.sleep(360);
+//        drivetrain.drive(0,0,0);
+//        Thread.sleep(10);
+        arm.MoveUp();
+        Thread.sleep(700);
+        arm.ArmStop();
+        Thread.sleep(10);
+        armTape.ArmMoveUp();
+        Thread.sleep(900);
+        armTape.AutoMoveUp();
+        Thread.sleep(500);
+        armTape.AutoArmTapeStop();
+        intake.IntakeOut();
+        Thread.sleep(2300);
+        intake.StopIntake();
+        Thread.sleep(50);
+        //Auto go back to normal
+        armTape.ArmMoveDown();
+        Thread.sleep(300);
+        armTape.AutoMoveDown();
+        Thread.sleep(360);
+        armTape.ArmTapeStop();
+        Thread.sleep(10);
+        arm.AutoMoveDown();
+        Thread.sleep(500);
+        arm.AutoArmStop();
+        Thread.sleep(2000);
+        drivetrain.drive(1, 0, 0);
+        Thread.sleep(500);
+        drivetrain.drive(-1, 0, 0);
+        Thread.sleep(500);
+
+
+        //intake.StopIntake();
+
+        /*
+        drivetrain.drive(0.1, 0);
+        Thread.sleep(500);
+        drivetrain.drive(0, 0);
+         */
+    }
+}
